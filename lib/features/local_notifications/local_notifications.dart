26d6927464bc2b3b8e224b4f1cae1d999369bfc9
Ahmed Wael <ahmedwael99104@gmail.com>
2025-06-25 00:10:10 +0300

    define alocal and push notification setup classes and methods

--- lib/features/local_notifications/local_notifications.dart
+++ lib/features/local_notifications/local_notifications.dart
@@ -1,1 +1,13 @@
-class LocalNotifications {}
+abstract class NotificationSetup {
+  void setup();
+}
+
+class LocalNotificationSetup implements NotificationSetup {
+  @override
+  void setup() {}
+}
+
+class PushNotificationSetup implements NotificationSetup {
+  @override
+  void setup() {}
+}
